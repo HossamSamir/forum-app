b5f24f1fe4fff04c6813016ff93c1ea24573324f
Hossam Samir <Hossameos1@gmail.com>
2017-08-09 19:37:09 +0200

    login screen

--- screens/Login.js
+++ screens/Login.js
@@ -8,8 +8,13 @@ import {
   TouchableOpacity,
   View,
   Button,
-  Alert
+  Alert,
+  TextInput,
+  KeyboardAvoidingView
 } from 'react-native';
+import { Col, Row, Grid } from "react-native-easy-grid"
+import { EvilIcons } from '@expo/vector-icons';
+
 
 export default class Login extends React.Component {
   static navigationOptions = {
@@ -18,17 +23,73 @@ export default class Login extends React.Component {
 
   render() {
     return (
-      <View style={styles.container}>
-        <Text>
-          asklfhaklshflkashf
-        </Text>
-        <Text>
-          asklfhaklshflkashf
-        </Text>
-        <Text>
-          asklfhaklshflkashf
-        </Text>
-      </View>
+        <KeyboardAvoidingView behavior='padding' style={styles.container}>
+
+            <Image source={require('../assets/images/bg.png')}
+            style={{
+              flex: 1,
+              position: 'absolute',
+              bottom: 0,
+              resizeMode: 'cover'
+            }}/>
+
+          <Grid>
+              <View style={{
+                alignItems: 'center',
+                justifyContent: 'center',
+                flex: 2,
+                }}>
+                <Image source={require('../assets/images/logo.png')}
+                  style={{
+                    flex: 1,
+                    marginTop: 50,
+                    resizeMode: 'contain',
+                  }} />
+              </View>
+
+              <Row style={{flex: 2, flexDirection: 'column', }}>
+                <View style={{flexDirection: 'row', flex: 1, justifyContent: 'flex-end', alignItems: 'center',}}>
+                  <EvilIcons name='user' size={38} color='crimson' style={{ backgroundColor: 'transparent' }} />
+                  <TextInput placeholderTextColor='white' placeholder="Email Address" style={{
+                  width: '80%',
+                  fontSize: 10,
+                  opacity: .7,
+                  fontSize: 18,
+                  color: 'white',
+                  borderBottomColor: 'crimson',
+                  borderBottomWidth: 1
+                }} />
+                </View>
+
+                <View style={{flexDirection: 'row', flex: 1, justifyContent: 'flex-end', alignItems: 'center',}}>
+                  <EvilIcons name='lock' size={38} color='crimson' style={{ backgroundColor: 'transparent' }} />
+                  <TextInput placeholderTextColor='white' placeholder="Password" secureTextEntry={true} style={{
+                  width: '80%',
+                  fontSize: 10,
+                  opacity: .7,
+                  fontSize: 18,
+                  color: 'white',
+                  borderBottomColor: 'crimson',
+                  borderBottomWidth: 1
+                }} />
+                </View>
+
+                <View style={{flexDirection: 'row', flex: 1, justifyContent: 'center', alignItems: 'center',}}>
+                  <TouchableOpacity style={{ backgroundColor: 'crimson', paddingHorizontal: 50, paddingVertical: 8, borderRadius: 25 }}>
+                    <EvilIcons name='arrow-right' size={25} color='white' />
+                  </TouchableOpacity>
+                </View>
+
+
+              </Row>
+
+              <Row style={{flex: .4, alignItems: 'center', justifyContent: 'center'}}>
+                <TouchableOpacity style={{ }}>
+                  <Text>Dont have an account? Sign up</Text>
+                </TouchableOpacity>
+              </Row>
+          </Grid>
+        </KeyboardAvoidingView>
     );
   }
 
@@ -38,6 +99,5 @@ export default class Login extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
   },
 });
