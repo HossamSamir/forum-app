cd43772194215135918f9ad4e8f27682b6951873
Hossam Samir <Hossameos1@gmail.com>
2017-08-11 21:36:28 +0200

    add post screen done

--- screens/AddPost.js
+++ screens/AddPost.js
@@ -10,7 +10,9 @@ import {
   Button,
   Alert,
   TextInput,
-  KeyboardAvoidingView
+  KeyboardAvoidingView,
+  Picker,
+  Item
 } from 'react-native';
 import { WebView } from 'react-native';
 import { ImagePicker } from 'expo';
@@ -21,7 +23,11 @@ export default class AddPost extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      image: 'https://support.e2ma.net/@api/deki/files/783/placeholder.png?revision=1&size=bestfit&width=468&height=186'
+      image: 'https://support.e2ma.net/@api/deki/files/783/placeholder.png?revision=1&size=bestfit&width=468&height=186',
+      category: '',
+      subcategory: '',
+      showCategoreis: false,
+      showsubCategoreis: false
     }
   }
 
@@ -39,6 +45,50 @@ export default class AddPost extends React.Component {
     }
   };
 
+    updateCategory = (category) => {
+      this.setState({ category: category })
+    }
+
+    updatesubCategory = (subcategory) => {
+      this.setState({ subcategory: subcategory })
+    }
+
+    _categories = () => {
+      if (this.state.showCategoreis) {
+        return (
+            <Picker style={styles.picker} selectedValue = {this.state.category} onValueChange = {this.updateCategory}>
+              <Picker.Item label = "Anime category 1" value = "Anime category 1" />
+              <Picker.Item label = "Anime category 2" value = "Anime category 2" />
+              <Picker.Item label = "Anime category 3" value = "Anime category 3" />
+              <Picker.Item label = "Anime category 4" value = "Anime category 4" />
+              <Picker.Item label = "Anime category 5" value = "Anime category 5" />
+              <Picker.Item label = "Anime category 6" value = "Anime category 6" />
+              <Picker.Item label = "Anime category 7" value = "Anime category 7" />
+              <Picker.Item label = "Anime category 8" value = "Anime category 8" />
+              <Picker.Item label = "Anime category 9" value = "Anime category 9" />
+            </Picker>
+        );
+      }
+    }
+
+    _subcategories = () => {
+      if (this.state.showsubCategoreis) {
+        return (
+            <Picker style={styles.picker} selectedValue = {this.state.subcategory} onValueChange = {this.updatesubCategory}>
+              <Picker.Item label = "Anime subcategory 1" value = "Anime subcategory 1" />
+              <Picker.Item label = "Anime subcategory 2" value = "Anime subcategory 2" />
+              <Picker.Item label = "Anime subcategory 3" value = "Anime subcategory 3" />
+              <Picker.Item label = "Anime subcategory 4" value = "Anime subcategory 4" />
+              <Picker.Item label = "Anime subcategory 5" value = "Anime subcategory 5" />
+              <Picker.Item label = "Anime subcategory 6" value = "Anime subcategory 6" />
+              <Picker.Item label = "Anime subcategory 7" value = "Anime subcategory 7" />
+              <Picker.Item label = "Anime subcategory 8" value = "Anime subcategory 8" />
+              <Picker.Item label = "Anime subcategory 9" value = "Anime subcategory 9" />
+            </Picker>
+        );
+      }
+    }
+
   static navigationOptions = {
     header: null,
   };
@@ -46,7 +96,7 @@ export default class AddPost extends React.Component {
   render() {
      let { image } = this.state;
     return (
-      <KeyboardAvoidingView behavior='padding' style={{ flex: 1 }}>
+      <KeyboardAvoidingView behavior='padding' style={{ flex: 1, backgroundColor: 'white' }}>
         <ScrollView>
 
         <TouchableOpacity onPress={this._pickImage}>
@@ -56,21 +106,80 @@ export default class AddPost extends React.Component {
 
 
           <TextInput
+            selectionColor='crimson'
+            returnKeyType = {"next"}
+            keyboardAppearance='dark'
+            autoFocus = {false}
+            onSubmitEditing={(event) => {
+              this.refs.descriptionInput.focus();
+            }}
+            autoCapitalize='characters'
             style={{
               fontSize: 30,
-              color: '#272727',
+              color: '#282828',
               marginTop: 18,
+              fontWeight: 'bold'
             }}
             placeholder="Type a title" />
 
           <TextInput
+            ref='descriptionInput'
+            keyboardAppearance='dark'
+            multiline={true}
+            selectionColor='crimson'
+            blurOnSubmit={true}
+            returnKeyType='done'
             style={{
               fontSize: 20,
-              color: '#272727',
+              color: '#808080',
+              lineHeight: 100,
               marginTop: 15,
               width: '100%'
             }}
             placeholder="Tell a story" />
+
+
+            <TouchableOpacity onPress={ () => this.setState({ showCategoreis: true }) }>
+              <Text style = {styles.category}> category:</Text>
+            </TouchableOpacity>
+
+            <Picker style={styles.picker} selectedValue = {this.state.category} onValueChange = {this.updateCategory}>
+              <Picker.Item label = "Anime category 1" value = "Anime category 1" />
+              <Picker.Item label = "Anime category 2" value = "Anime category 2" />
+              <Picker.Item label = "Anime category 3" value = "Anime category 3" />
+              <Picker.Item label = "Anime category 4" value = "Anime category 4" />
+              <Picker.Item label = "Anime category 5" value = "Anime category 5" />
+              <Picker.Item label = "Anime category 6" value = "Anime category 6" />
+              <Picker.Item label = "Anime category 7" value = "Anime category 7" />
+              <Picker.Item label = "Anime category 8" value = "Anime category 8" />
+              <Picker.Item label = "Anime category 9" value = "Anime category 9" />
+            </Picker>
+
+
+
+
+
+            <TouchableOpacity onPress={ () => this.setState({ showCategoreis: true }) }>
+              <Text style = {styles.category}> subcategory:</Text>
+            </TouchableOpacity>
+
+            <Picker style={styles.picker} selectedValue = {this.state.category} onValueChange = {this.updateCategory}>
+              <Picker.Item label = "Anime category 1" value = "Anime category 1" />
+              <Picker.Item label = "Anime category 2" value = "Anime category 2" />
+              <Picker.Item label = "Anime category 3" value = "Anime category 3" />
+              <Picker.Item label = "Anime category 4" value = "Anime category 4" />
+              <Picker.Item label = "Anime category 5" value = "Anime category 5" />
+              <Picker.Item label = "Anime category 6" value = "Anime category 6" />
+              <Picker.Item label = "Anime category 7" value = "Anime category 7" />
+              <Picker.Item label = "Anime category 8" value = "Anime category 8" />
+              <Picker.Item label = "Anime category 9" value = "Anime category 9" />
+            </Picker>
+
+
+
+
+
+
 
         </ScrollView>
       </KeyboardAvoidingView>
@@ -82,7 +191,18 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: 'column',
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
     paddingTop: 40
   },
+  category: {
+      fontSize: 20,
+      marginTop: 50,
+      marginBottom: -100
+   },
+  subcategorystyle: {
+      fontSize: 20,
+      marginBottom: -150
+   },
+   picker: {
+   }
 });
