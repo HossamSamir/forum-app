180ad21513732de3f41099cc36ab97efa5084a53
Hossam Samir <Hossameos1@gmail.com>
2017-08-12 16:19:08 +0200

    basic notifcation screen

--- screens/Notifications.js
+++ screens/Notifications.js
@@ -15,17 +15,45 @@ import { StackNavigator } from 'react-navigation';
 
 export default class Notifications extends React.Component {
 
-  static navigationOptions = {
+  constructor(props) {
+    super(props);
+    this.state = {
+      NotsExists: false
+    }
+  }
+
+
+    _handleShowNotifications = () => {
+      Alert.alert('asf')
+    }
+
+    _NotificationsPlaceholder = () => {
+      if (this.state.NotsExists == true) {
+        return (
+        console.log('true')
+      )
+      } else {
+        return (
+          <View style={styles.container}>
+            <Image source={require('../assets/images/illustration.jpg')} style={{ width: 150, height: 150 }} />
+            <Text style={{ color: '#9a9b9f', fontWeight: 'bold', fontSize: 23 }}>Nothing here!!!</Text>
+            <Text style={{ textAlign: 'center', color: '#d1d1d1', fontSize: 16, marginTop: 15 }}>Tab the notifications button below to add a fake test notifications to check them</Text>
+            <TouchableOpacity onPress={this._handleShowNotifications}>
+              <Text style={{ color: 'white', backgroundColor: '#ff717f', marginTop: 20, borderRadius: 8, paddingHorizontal: 20, paddingVertical: 10 }}>Fake Notifications</Text>
+            </TouchableOpacity>
+          </View>
+        );
+      }
+    }
+
+    static navigationOptions = {
       header: null
     };
-
 
   render() {
     return (
       <View style={styles.container}>
-        <Image source={require('../assets/images/illustration.jpg')} style={{ width: 150, height: 150 }} />
-        <Text style={{ color: '#9a9b9f', fontWeight: 'bold', fontSize: 23 }}>Nothing here!!!</Text>
-        <Text style={{ color: '#d1d1d1', fontSize: 13 }}>Tab the notifications button below to add a fake test notifications to check them</Text>
+        { this._NotificationsPlaceholder() }
       </View>
     );
   }
@@ -39,5 +67,6 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 20/2
   }
 });
