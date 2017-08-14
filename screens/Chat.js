cee2e2d46ec787ec85585bc625500e9c01b14828
Hossam Samir <Hossameos1@gmail.com>
2017-08-14 15:11:07 +0200

    chat page structure

--- screens/Chat.js
+++ screens/Chat.js
@@ -7,58 +7,109 @@ import {
   TouchableOpacity,
   View,
   Alert,
-  ListView,
-  Text
+  ListView
 } from 'react-native';
+import { Container, Header, Content, Button, Icon, List, ListItem, Text, Left, Body, Right, Thumbnail } from 'native-base';
 import { StackNavigator } from 'react-navigation';
-import { GiftedChat } from 'react-native-gifted-chat';
 
-export default class Notifications extends Component {
+const datas = [
+  'Hossam Samir',
+  'Ahmed Clyne',
+  'Dejan Lovren',
+  'Alberto Moreno',
+  'Emre Can',
+  'Joe Allen',
+  'Phil Coutinho',
+];
+
+export default class Chat extends Component {
 
   constructor(props) {
     super(props);
+    this.ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     this.state = {
-      messages: [],
+      basic: true,
+      listViewData: datas,
+      showChat: false
     };
   }
 
-  componentWillMount() {
-    this.setState({
-      messages: [
-        {
-          _id: 1,
-          text: 'Hello Admin 👍🏻💥 !',
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'Developer',
-            avatar: 'https://avatars1.githubusercontent.com/u/15352675?v=4&s=460',
-          },
-        },
-      ],
-    });
+  deleteRow(secId, rowId, rowMap) {
+    rowMap[`${secId}${rowId}`].props.closeRow();
+    const newData = [...this.state.listViewData];
+    newData.splice(rowId, 1);
+    this.setState({ listViewData: newData });
   }
 
-  onSend(messages = []) {
-    this.setState((previousState) => ({
-      messages: GiftedChat.append(previousState.messages, messages),
-    }));
-  }
+  _handleShowChat = () => {
+     this.setState({
+       showChat: true
+     });
+   }
 
+   _NotificationsPlaceholder = () => {
+      if (this.state.showChat == true) {
+        return (
+          <View style={styles.container}>
+            <Image source={require('../assets/images/illustration.jpg')} style={{ width: 150, height: 150 }} />
+            <Text style={{ color: '#9a9b9f', fontWeight: 'bold', fontSize: 23 }}>Nothing here!!!</Text>
+            <Text style={{ textAlign: 'center', color: '#d1d1d1', fontSize: 16, marginTop: 15 }}>Tab the notifications button below to add a fake test notifications to check them</Text>
+            <TouchableOpacity onPress={this._handleShowChat}>
+              <Text style={{ color: 'white', backgroundColor: '#ff717f', marginTop: 20, borderRadius: 8, paddingHorizontal: 20, paddingVertical: 10 }}>Fake Notifications</Text>
+            </TouchableOpacity>
+          </View>
+      )
+      } else {
+        return (
+          <View style={{
+                flex: 1,
+                flexDirection: 'row',
+              }}>
+              <List
+                style={{
+                  width: '100%',
+                  marginTop: 20,
+                  }}
+                dataSource={this.ds.cloneWithRows(this.state.listViewData)}
+                renderRow={data =>
+                  <ListItem avatar style={{
+                    width: '100%',
+                    marginVertical: 6,
+                    }}>
+                    <Left>
+                      <Thumbnail source={{ uri: 'https://avatars1.githubusercontent.com/u/15352675?v=4&s=460' }} />
+                    </Left>
+                    <Body>
+                      <Text> {data} </Text>
+                      <Text note> Liked Your Post</Text>
+                    </Body>
+                    <Right>
+                      <Text note>8 mins ago</Text>
+                    </Right>
+                  </ListItem>}
+                renderLeftHiddenRow={data => {}}
+                renderRightHiddenRow={(data, secId, rowId, rowMap) =>
+                  <Button style={{ marginVertical: 6 }} full danger onPress={_ => this.deleteRow(secId, rowId, rowMap)}>
+                    <Icon active name="trash" />
+                  </Button>}
+                leftOpenValue={.1}
+                rightOpenValue={-75}
+              />
+        </View>
+        );
+      }
+    }
 
     static navigationOptions = {
       header: null
     };
 
   render() {
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
     return (
-        <GiftedChat
-        messages={this.state.messages}
-        onSend={(messages) => this.onSend(messages)}
-        user={{
-          _id: 1,
-        }} />
-
+      <View style={styles.container}>
+        { this._NotificationsPlaceholder() }
+      </View>
     );
   }
 }
